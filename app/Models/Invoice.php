96757a58ef32057baf914e107ae0d660fed1023f
ddnguyen1120 <ducnguyendoan010120@gmail.com>
2023-05-31 15:37:23 +0700

    Create models

---
+++ app/Models/Invoice.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    protected $table = 'invoices';
+
+    protected $fillable = [
+        'invoice_name',
+        'invoice_code',
+        'invoice_description',
+        'invoice_status'
+    ];
+}
